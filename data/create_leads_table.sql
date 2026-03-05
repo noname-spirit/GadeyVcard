775ee450aaefcf5e82f31e1d293341b7aec37a06
noname <bonjour@noname-spirit.com>
2026-03-05 18:17:38 +0700

    database connection neon

---
+++ data/create_leads_table.sql
@@ -0,0 +1,9 @@
+-- SQL pour créer la table leads
+CREATE TABLE IF NOT EXISTS leads (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telephone VARCHAR(30) NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+    source VARCHAR(50) NOT NULL
+);
