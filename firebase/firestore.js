c42789fe7e3ac355a4ddc58b4153e14676f637b6
heru97129 <beaujourgregory@gmail.com>
2026-04-22 01:15:26 +0200

    `Migrate from Supabase to Firebase`

--- firebase/firestore.js
+++ firebase/firestore.js
@@ -1,12 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// /jaklxhjaksxhakxhjlkjh
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyDZar-nOYC7X5AtffYVqQPX4uVr9a4XE54",
   authDomain: "generateur-vcard.firebaseapp.com",
@@ -14,10 +9,7 @@ const firebaseConfig = {
   storageBucket: "generateur-vcard.firebasestorage.app",
   messagingSenderId: "187676946208",
   appId: "1:187676946208:web:2db51f14d09d8649687145",
-  measurementId: "G-T3C5CH4DGY"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
